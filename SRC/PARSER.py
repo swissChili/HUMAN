371a697c29769c3073a5bbb28c68fbc37044eaa1
swissChili <sasha@fastmail.com>
2018-10-13 12:47:07 -0700

    updated parser slightly

--- SRC/PARSER.py
+++ SRC/PARSER.py
@@ -4,40 +4,60 @@ import inspect
 stack = []
 
 def stack_push(i):
-	stack.append(i)
+    stack.append(i)
 
 def stack_list():
-	return " ".join(stack)
+    return " ".join(stack)
 
-def return_string():
-	return "string"
+def return_string(given_string):
+    return given_string
 
 def say(i):
-	print(i)
-	print('SAID')
+    print(i)
 
 funcs = {
-	r"^STACK\.PUSH\s*\((.+)\)$": stack_push,
-	r"^SAY\s*\((.+)\)$": say,
-	r"^STACK\.LIST\s*$": stack_list,
-	r'^\s*\"(.+)\"\s*$': return_string
+    r"^\s*STACK\.PUSH\s*\((.+)\)\s*$": {
+        "function": stack_push,
+        "recursion_level": 1
+    },
+    r"^\s*SAY\s*\((.+)\)\s*$": {
+        "function": say,
+        "recursion_level": 1
+    },
+    r"^\s*STACK\.LIST\s*$": {
+        "function": stack_list,
+        "recursion_level": 0
+    },
+    r"^\s*\".+\"\s*$": {
+        "function": return_string,
+        "recursion_level": 0
+    }
 }
 
 def parse_line(id, line):
-	for key in funcs:
-		res = re.findall(key, line)
-		#print("TRYING: {}".format(funcs[key]))
-		print("stack",stack)
-		try:
-			if len(inspect.getargspec(funcs[key]).args) == 1:
-				funcs[key](parse_line(id, res[0]))
-			else:
-				return funcs[key]()
+    for key in funcs:
+        res = re.findall(key, line)
+        print("TRYING: {}".format(funcs[key]))
+        print("stack",stack)
+        try:
+            # inspect the signature of the function to see what
+            # parameters it takes
+            signature = len(inspect.signature(funcs[key]["function"]).parameters)
+            if signature > 0:
+                print("takes some parameters")
+                if funcs[key]["recursion_level"] > 0:
+                    print("attempting to recursively parse function", res[0])
+                    funcs[key]["function"](parse_line(id, res[0]))
+                else:
+                    print("attempting to feed regex to function", res[0])
+                    funcs[key]["function"](res[0])
+            else:
+                funcs[key]["function"]
 
-		except IndexError:
-			print("ERROR ON LINE {}: '{}' IS NOT DEFINED".format(id, line))
+        except IndexError:
+            print("ERROR ON LINE {}: '{}' IS NOT DEFINED".format(id, line))
 
 def parse ( program ):
-	program = program.split('\n')
-	for id, line in enumerate(program):
-		parse_line(id, line)
+    program = program.split('\n')
+    for id, line in enumerate(program):
+        parse_line(id, line)
